d8f9b5541ae5c5770a381004ef03bc2476e6e1da
installuser <installuser@NM355771.tw.sinyi.com>
2019-09-27 11:18:04 +0800

    homework

---
+++ pages/sample1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Head from 'next/head'
+import Nav from '../components/nav'
+
+const sample = () => (
+    <>
+    <h1>wellcome</h1>
+    </>
+)
+
+export default sample
